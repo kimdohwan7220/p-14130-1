191d888004007889ab960da9304e2f6a6417e4ae
kimdohwan7220 <dohwan8220@naver.com>
2026-02-05 11:30:46 +0900

    refactor: 10강, 목록

--- src/main/kotlin/com/ll/App.kt
+++ src/main/kotlin/com/ll/App.kt
@@ -5,14 +5,14 @@ class App {
         println("== 명언 앱 ==")
 
         var lastId = 0
+        val wiseSayings = mutableListOf<WiseSaying>();
 
         while (true) {
-            print("명언) ")
+            print("명령) ")
 
             val input = readlnOrNull()!!.trim()
 
-            if (input == "종료")
-                break
+            if (input == "종료") break
             else if (input == "등록") {
                 print("명언 : ")
                 val content = readlnOrNull()!!.trim()
@@ -21,7 +21,22 @@ class App {
 
                 val id = ++lastId
 
+                wiseSayings.add(WiseSaying(id, content, author))
+
                 println("${id}번 명언이 등록되었습니다.")
+            } else if (input == "목록") {
+                if (wiseSayings.isEmpty()) {
+                    println("등록된 명언이 없습니다.")
+                    continue
+                }
+
+                println("번호 / 작가 / 명언")
+
+                println("----------------------")
+
+                wiseSayings.forEach {
+                    println("${it.id} / ${it.author} / ${it.content}")
+                }
             }
         }
     }
